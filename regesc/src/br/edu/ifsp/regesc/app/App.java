db49438e6043f11e1151645f48df9664b28e88ff
sbmmartins <sbmmartins@gmail.com>
2016-09-16 22:01:38 -0300

    findAll regesc

--- regesc/src/br/edu/ifsp/regesc/app/App.java
+++ regesc/src/br/edu/ifsp/regesc/app/App.java
@@ -3,18 +3,32 @@ package br.edu.ifsp.regesc.app;
 
 import br.edu.ifsp.regesc.dao.StudentDAO;
 import br.edu.ifsp.regesc.models.Student;
+import java.util.ArrayList;
 
 
 public class App {
     public static void main(String[] args) {     
-        Student student = new Student("Tiririca Abestado", 50);
-        System.out.println(student);
-        
+//        Student student = new Student("Bolsonaro Mito!", 20);
+//        System.out.println(student);
+//        
+//        StudentDAO dao = new StudentDAO();
+//        dao.insert(student);
+//        
+//        System.out.println(student);
+//        
+//        System.out.println("Deu bom!");
+
         StudentDAO dao = new StudentDAO();
-        dao.insert(student);
+        ArrayList<Student> studentList = dao.findAll();
         
-        System.out.println(student);
         
-        System.out.println("Deu bom!");
+        System.out.println("*******************");
+        
+        for (Student student : studentList) {
+            System.out.println("----");
+            System.out.println(student);
+            System.out.println("----");
+        }
+        System.out.println("*******************");
     }
 }
